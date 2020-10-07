9b423b60c3ee9caa936e1660b1b14a67197f992d
vilture <leemon.rdtc@gmail.com>
2020-10-07 17:18:12 +0700

    first commit

--- app/src/main/java/ru/vilture/aquahemist/MainActivity.kt
+++ app/src/main/java/ru/vilture/aquahemist/MainActivity.kt
@@ -66,6 +66,7 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             ) {
                 rsPH(ph)
                 rsNH3(nh3)
+                co2()
             }
         })
         kh.addTextChangedListener(object : TextWatcher {
@@ -81,6 +82,7 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
                 before: Int, count: Int
             ) {
                 rsKH(kh)
+                co2()
             }
         })
         gh.addTextChangedListener(object : TextWatcher {
@@ -159,7 +161,11 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             }
         })
 
-
+        // обновление экрана
+        swipe_refresh.setOnRefreshListener {
+            swipe_refresh.isRefreshing = true
+            swipe_refresh.isRefreshing = false
+        }
     }
 
 
@@ -225,6 +231,7 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
                 rsNO3(no3)
                 rsPO4(po4)
                 rsNH3(nh3)
+                co2()
             }
         }
     }
@@ -240,13 +247,16 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
         values["nh3"] = nh3.text.toString()
         values["co2"] = co2.text.toString()
 
+
+
         val resSave =
             DBHandler(this).addData(values)
-        existDate()
         if (!resSave.isOk)
             Toast.makeText(this, resSave.message, Toast.LENGTH_LONG).show()
         else
             Toast.makeText(this, "Сохранено", Toast.LENGTH_LONG).show()
+        existDate()
+        mcv.invalidateDecorators()
     }
 
     private fun existDate() {
@@ -301,9 +311,9 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             return
         }
         val double = ph.text.toString().toDouble()
-        if (double >= 7.4)      rsPH.setBackgroundResource(R.color.orange)
-        if (double >= 8.0)      rsPH.setBackgroundResource(R.color.red)
-        if (double <= 6.7)      rsPH.setBackgroundResource(R.color.yellow)
+        if (double >= 7.4) rsPH.setBackgroundResource(R.color.orange)
+        if (double >= 8.0) rsPH.setBackgroundResource(R.color.red)
+        if (double <= 6.7) rsPH.setBackgroundResource(R.color.yellow)
         if (double in 6.8..7.3) rsPH.setBackgroundResource(R.color.green)
     }
 
@@ -314,9 +324,9 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             return
         }
         val double = kh.text.toString().toDouble()
-        if (double >= 6.1)      rsKH.setBackgroundResource(R.color.orange)
-        if (double >= 12.0)     rsKH.setBackgroundResource(R.color.red)
-        if (double <= 2.9)      rsKH.setBackgroundResource(R.color.yellow)
+        if (double >= 6.1) rsKH.setBackgroundResource(R.color.orange)
+        if (double >= 12.0) rsKH.setBackgroundResource(R.color.red)
+        if (double <= 2.9) rsKH.setBackgroundResource(R.color.yellow)
         if (double in 3.0..6.0) rsKH.setBackgroundResource(R.color.green)
     }
 
@@ -327,9 +337,9 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             return
         }
         val double = gh.text.toString().toDouble()
-        if (double >= 8.1)      rsGH.setBackgroundResource(R.color.orange)
-        if (double >= 10.0)     rsGH.setBackgroundResource(R.color.red)
-        if (double <= 3.9)      rsGH.setBackgroundResource(R.color.yellow)
+        if (double >= 8.1) rsGH.setBackgroundResource(R.color.orange)
+        if (double >= 10.0) rsGH.setBackgroundResource(R.color.red)
+        if (double <= 3.9) rsGH.setBackgroundResource(R.color.yellow)
         if (double in 4.0..8.0) rsGH.setBackgroundResource(R.color.green)
     }
 
@@ -340,9 +350,9 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             return
         }
         val double = no2.text.toString().toDouble()
-        if (double >= 0.3)      rsNO2.setBackgroundResource(R.color.orange)
-        if (double >= 2.0)      rsNO2.setBackgroundResource(R.color.red)
-        if (double == 0.0)      rsNO2.setBackgroundResource(R.color.yellow)
+        if (double >= 0.3) rsNO2.setBackgroundResource(R.color.orange)
+        if (double >= 2.0) rsNO2.setBackgroundResource(R.color.red)
+        if (double == 0.0) rsNO2.setBackgroundResource(R.color.yellow)
         if (double in 0.1..0.5) rsNO2.setBackgroundResource(R.color.green)
     }
 
@@ -353,10 +363,10 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             return
         }
         val double = no3.text.toString().toDouble()
-        if (double >= 5.1 )      rsNO3.setBackgroundResource(R.color.yellow)
-        if (double >= 10.1)      rsNO3.setBackgroundResource(R.color.orange)
-        if (double >= 40.0)      rsNO3.setBackgroundResource(R.color.red)
-        if (double in 0.0..5.0)  rsNO3.setBackgroundResource(R.color.green)
+        if (double >= 5.1) rsNO3.setBackgroundResource(R.color.yellow)
+        if (double >= 10.1) rsNO3.setBackgroundResource(R.color.orange)
+        if (double >= 40.0) rsNO3.setBackgroundResource(R.color.red)
+        if (double in 0.0..5.0) rsNO3.setBackgroundResource(R.color.green)
     }
 
     private fun rsPO4(po4: EditText?) {
@@ -366,9 +376,9 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             return
         }
         val double = po4.text.toString().toDouble()
-        if (double <=0.4 )      rsPO4.setBackgroundResource(R.color.yellow)
-        if (double >= 2.1)      rsPO4.setBackgroundResource(R.color.orange)
-        if (double >= 5.0)      rsPO4.setBackgroundResource(R.color.red)
+        if (double <= 0.4) rsPO4.setBackgroundResource(R.color.yellow)
+        if (double >= 2.1) rsPO4.setBackgroundResource(R.color.orange)
+        if (double >= 5.0) rsPO4.setBackgroundResource(R.color.red)
         if (double in 0.5..2.0) rsPO4.setBackgroundResource(R.color.green)
     }
 
@@ -379,136 +389,296 @@ class MainActivity : AppCompatActivity(), OnDateSelectedListener, OnMonthChanged
             return
         }
         val double = nh3.text.toString().toDouble()
-        if (double == 0.0 ) {
+        if (double == 0.0) {
             rsNH3.setBackgroundResource(R.color.white)
             rsNH3.text = "0.000"
         }
 
-        if (double in 0.1..1.9){
-            if (ph.text.toString().toDouble() in 0.0..7.4){
+        if (double in 0.1..1.9) {
+            if (ph.text.toString().toDouble() in 0.0..7.4) {
                 rsNH3.setBackgroundResource(R.color.white)
                 rsNH3.text = "0.006"
             }
-            if (ph.text.toString().toDouble() in 7.5..7.9){
+            if (ph.text.toString().toDouble() in 7.5..7.9) {
                 rsNH3.setBackgroundResource(R.color.white)
                 rsNH3.text = "0.018"
             }
-            if (ph.text.toString().toDouble() in 8.0..8.4){
+            if (ph.text.toString().toDouble() in 8.0..8.4) {
                 rsNH3.setBackgroundResource(R.color.yellow)
                 rsNH3.text = "0.054"
             }
-            if (ph.text.toString().toDouble() in 8.5..8.9){
+            if (ph.text.toString().toDouble() in 8.5..8.9) {
                 rsNH3.setBackgroundResource(R.color.yellow)
                 rsNH3.text = "0.154"
             }
-            if (ph.text.toString().toDouble() in 9.0..9.4){
+            if (ph.text.toString().toDouble() in 9.0..9.4) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.365"
             }
-            if (ph.text.toString().toDouble() in 9.5..9.9){
+            if (ph.text.toString().toDouble() in 9.5..9.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.645"
             }
-            if (ph.text.toString().toDouble() >= 10){
+            if (ph.text.toString().toDouble() >= 10) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = ">0.852"
             }
         }
 
 
-        if (double in 2.0..4.9){
-            if (ph.text.toString().toDouble() in 0.0..7.4){
+        if (double in 2.0..4.9) {
+            if (ph.text.toString().toDouble() in 0.0..7.4) {
                 rsNH3.setBackgroundResource(R.color.white)
                 rsNH3.text = "0.014"
             }
-            if (ph.text.toString().toDouble() in 7.5..7.9){
+            if (ph.text.toString().toDouble() in 7.5..7.9) {
                 rsNH3.setBackgroundResource(R.color.yellow)
                 rsNH3.text = "0.036"
             }
-            if (ph.text.toString().toDouble() in 8.0..8.4){
+            if (ph.text.toString().toDouble() in 8.0..8.4) {
                 rsNH3.setBackgroundResource(R.color.yellow)
                 rsNH3.text = "0.109"
             }
-            if (ph.text.toString().toDouble() in 8.5..8.9){
+            if (ph.text.toString().toDouble() in 8.5..8.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.308"
             }
-            if (ph.text.toString().toDouble() in 9.0..9.4){
+            if (ph.text.toString().toDouble() in 9.0..9.4) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.731"
             }
-            if (ph.text.toString().toDouble() in 9.5..9.9){
+            if (ph.text.toString().toDouble() in 9.5..9.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "1.230"
             }
-            if (ph.text.toString().toDouble() >= 10){
+            if (ph.text.toString().toDouble() >= 10) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = ">1.704"
             }
         }
 
-        if (double in 5.0..9.9){
-            if (ph.text.toString().toDouble() in 0.0..7.4){
+        if (double in 5.0..9.9) {
+            if (ph.text.toString().toDouble() in 0.0..7.4) {
                 rsNH3.setBackgroundResource(R.color.yellow)
                 rsNH3.text = "0.029"
             }
-            if (ph.text.toString().toDouble() in 7.5..7.9){
+            if (ph.text.toString().toDouble() in 7.5..7.9) {
                 rsNH3.setBackgroundResource(R.color.yellow)
                 rsNH3.text = "0.090"
             }
-            if (ph.text.toString().toDouble() in 8.0..8.4){
+            if (ph.text.toString().toDouble() in 8.0..8.4) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.272"
             }
-            if (ph.text.toString().toDouble() in 8.5..8.9){
+            if (ph.text.toString().toDouble() in 8.5..8.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.770"
             }
-            if (ph.text.toString().toDouble() in 9.0..9.4){
+            if (ph.text.toString().toDouble() in 9.0..9.4) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "1.827"
             }
-            if (ph.text.toString().toDouble() in 9.5..9.9){
+            if (ph.text.toString().toDouble() in 9.5..9.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "3.227"
             }
-            if (ph.text.toString().toDouble() >= 10){
+            if (ph.text.toString().toDouble() >= 10) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = ">4.260"
             }
         }
 
-        if (double >= 10.0){
-            if (ph.text.toString().toDouble() in 0.0..7.4){
+        if (double >= 10.0) {
+            if (ph.text.toString().toDouble() in 0.0..7.4) {
                 rsNH3.setBackgroundResource(R.color.yellow)
                 rsNH3.text = "0.057"
             }
-            if (ph.text.toString().toDouble() in 7.5..7.9){
+            if (ph.text.toString().toDouble() in 7.5..7.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.179"
             }
-            if (ph.text.toString().toDouble() in 8.0..8.4){
+            if (ph.text.toString().toDouble() in 8.0..8.4) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "0.544"
             }
-            if (ph.text.toString().toDouble() in 8.5..8.9){
+            if (ph.text.toString().toDouble() in 8.5..8.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "1.540"
             }
-            if (ph.text.toString().toDouble() in 9.0..9.4){
+            if (ph.text.toString().toDouble() in 9.0..9.4) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "3.653"
             }
-            if (ph.text.toString().toDouble() in 9.5..9.9){
+            if (ph.text.toString().toDouble() in 9.5..9.9) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = "6.454"
             }
-            if (ph.text.toString().toDouble() >= 10){
+            if (ph.text.toString().toDouble() >= 10) {
                 rsNH3.setBackgroundResource(R.color.red)
                 rsNH3.text = ">8.529"
             }
         }
 
+    }
+
+    private fun co2() {
+        if (kh?.text!!.isEmpty() || ph?.text!!.isEmpty()) {
+            co2.setBackgroundColor(Color.TRANSPARENT)
+            co2.text.append("Введите pH и kH значения")
+            return
+        }
+
+        co2.text.clear()
+        val dph = ph.text.toString().toDouble()
+        val dkh = kh.text.toString().toDouble()
+
+        if (dkh in 0.0..0.9) {
+            if (dph <= 6.1) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            } else {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 1.0..1.9) {
+            if (dph <= 6.3) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            } else {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 2.0..2.9) {
+            if (dph <= 6.3) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 6.4..6.6) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 6.7) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 3.0..3.9) {
+            if (dph <= 6.3) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 6.4..6.9) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 7.0) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 4.0..5.9) {
+            if (dph <= 6.6) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 6.7..7.1) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 7.2) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 6.0..6.9) {
+            if (dph <= 6.9) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 7.0..7.1) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 7.2) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 7.0..10.9) {
+            if (dph <= 6.9) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 7.0..7.3) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 7.4) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 11.0..12.9) {
+            if (dph <= 7.1) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 7.2..7.3) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 7.4) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh in 13.0..18.9) {
+            if (dph <= 7.1) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 7.2..7.7) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 7.8) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
+
+        if (dkh >= 19.0) {
+            if (dph <= 7.3) {
+                co2.setBackgroundResource(R.color.red)
+                co2.text.append("Много")
+
+            }
+            if (dph in 7.4..7.7) {
+                co2.setBackgroundResource(R.color.green)
+                co2.text.append("Оптимально")
+            }
+            if (dph >= 7.8) {
+                co2.setBackgroundResource(R.color.blue)
+                co2.text.append("Мало")
+            }
+        }
     }
 
 }
